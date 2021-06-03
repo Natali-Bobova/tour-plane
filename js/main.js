c93890d60a22b6fd59435e9bd82871c63c3100ef
Natali-Bobova <natalibobova0@gmail.com>
2021-06-03 20:29:53 +0200

    create: addet section reviews, fix hotel-slider

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
-const swiper = new Swiper('.swiper-container', {
+const hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    prevEl: '.slider-button--prev',
-    nextEl: '.slider-button--next', 
+    prevEl: '.hotel-slider__button--prev',
+    nextEl: '.hotel-slider__button--next', 
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
+const reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    prevEl: '.reviews-slider__button--prev',
+    nextEl: '.reviews-slider__button--next', 
   },
 
   keyboard: {
